ef85526acab853b35770fb76189565b995bab331
Shayan Raza <shayanraza07@gmail.com>
2022-07-14 23:46:29 +0300

    A button that refreshes data

--- webapp.py
+++ webapp.py
@@ -5,12 +5,11 @@ import matplotlib as plt
 import subprocess
 import sys
 
-#Creating a button if clicked refreshes the data
-
 st.title("Crypto currency web-app Data") #Title of the webpage
 
 df = pd.read_csv("/home/shayan/Desktop/Code/Data Science/Cryptocurrency-data-web-app/Data.csv") #Importing our CSV
 
+#Creating a button that refreshes the data by running the script
 if st.button("Refresh Data") : 
     subprocess.run([f"{sys.executable}", "data.py"]) #Runs the script again to get the latest data
 else : 
